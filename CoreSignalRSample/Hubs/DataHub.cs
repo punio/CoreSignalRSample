c205e818e2b068c1d3b7827ef86684b31b367b4f
羽田 佳啓 <punio@outlook.com>
2018-07-05 11:01:08 +0900

    プロジェクト ファイルを追加します。

---
+++ CoreSignalRSample/Hubs/DataHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace CoreSignalRSample.Hubs
+{
+	public class DataHub : Hub
+	{
+		public void Upload(byte[] data)
+		{
+			System.Diagnostics.Debug.WriteLine($"Receive {data.Length}bytes.");
+		}
+	}
+}
---
+++ TestClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TestClient
+{
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
+			var connection = new HubConnectionBuilder()
+				.WithUrl("http://localhost:58456/hub/data")
+				.AddMessagePackProtocol()
+				.Build();
+			await connection.StartAsync();
+
+			var testData = Enumerable.Range(0, 1024).Select(i => (byte)i).ToArray();
+			await connection.InvokeAsync("Upload", testData);
+
+			await connection.DisposeAsync();
+		}
+	}
+}
